233fcd6740d5b0ae116e949e7abc563dfd0fd449
Sakib-05 <sakibhoque@Sakibs-MacBook-Pro.local>
2024-12-09 10:42:18 +0000

    this is the start of the CUoL ISOC website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+      this is the start of the ISOC website
+    </div>
+    
+  )
+}
+
+export default App
