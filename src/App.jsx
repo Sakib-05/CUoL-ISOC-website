7f6542fde444f1ac676308032ca106e06dbabf7a
Sakib-05 <sakibimamul03@gmail.com>
2024-12-09 10:49:26 +0000

    project is now ready for development

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="app">
       this is the start of the ISOC website
-      {/* test comment */}
-      {/* another test commit */}
     </div>
     
   )
