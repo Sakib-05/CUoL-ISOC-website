8d8b1df6387b1803a4901b1d8428274ec0485499
YaqubRahman <YaqubRahman999@gmail.com>
2024-12-19 10:04:16 +0000

    made corresponding folders for pages and others and made home page look slightly nicer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
 
   return (
-    <div className="app">
+    <><div className="app">
       this is the start of the ISOC website
     </div>
     
+    <Routes>
+      <Route path="/" element ={<Home />}/>
+      </Routes></>
+
+
   )
 }
 
