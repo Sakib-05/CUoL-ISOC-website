4e4f1fd9375b48e58592a2b48f612fa9c5f431ff
YaqubRahman <YaqubRahman999@gmail.com>
2024-12-20 14:31:42 +0000

    added images and tried formating the page, still needs work

--- src/components/MainPageImages.jsx
+++ src/components/MainPageImages.jsx
@@ -1,12 +1,29 @@
 import ISOCLogo from  '../assets/ISOC logo HighRes.jpg'
+import CityBanner from '../assets/CityBanner3.jpg'
 import '../components/MainPageImages.css'
 
 function MainPageImages(){
     return(
+        <div>
+
+        <div class = "">
+        <img src ={CityBanner} className="citybanner" alt ="citybanner" />
+        </div>
 
 
-        <div class="parent-container">
+
+
+        <div class="flex-container">
+
+        <p className='whoarewe'>Who are we</p>
+
+        <p className='whoarewedescription'> We are muslims from the<br></br> City University of London<br></br> who are part of a society (this is all test)</p>   
+
+
         <img src ={ISOCLogo} className="isocmainlogo" alt ="isoclogo" />
+        </div>
+
+
         </div>
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,16 @@ function Home(){
     return(
         <>
         <div>
-        <h1 className='isocmainheader '>السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ</h1>
+
+        <div className='container'>
+        <h1 className='salaamsheader '>السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ</h1>
         <h2 className='isocmainheader'>City University of London ISOC</h2>         
         </div>
-
+        </div>      
         
         <MainPageImages/>
-        
+    
+    
         
         
         </>
